6b98a884fae6d75dd72020839c82fd026a2bf250
Jeanine Harb <jeanine.harb@gmail.com>
2016-07-25 19:14:06 +0300

    Created products view

---
+++ resources/views/products/noreva.blade.php
@@ -0,0 +1,140 @@
+@extends('structure')
+
+@section('content')
+
+<section class="mbr-section mbr-section__container mbr-after-navbar mbr-section__container--isolated" id="header3-19">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h3 class="mbr-section-title display-2">Laboratoires NOREVA</h3>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-gallery mbr-section mbr-section-nopadding" id="gallery1-22" style="background-color: rgb(239, 239, 239); padding-top: 0px; padding-bottom: 0px;">
+    <!-- Gallery -->
+    <div>
+        <div class=" mbr-gallery-layout-default">
+            <div>
+                <div>
+                    <div class="mbr-gallery-item">
+                        <a href="#lb-gallery1-22" data-slide-to="0" data-toggle="modal">
+                            <img alt="" src="assets/images/bike-small.jpg">
+                            <span class="icon"></span>
+                            
+                        </a>
+                    </div><div class="mbr-gallery-item">
+                        <a href="#lb-gallery1-22" data-slide-to="1" data-toggle="modal">
+                            <img alt="" src="assets/images/code-man-small.jpg">
+                            <span class="icon"></span>
+                            
+                        </a>
+                    </div><div class="mbr-gallery-item">
+                        <a href="#lb-gallery1-22" data-slide-to="2" data-toggle="modal">
+                            <img alt="" src="assets/images/coworkers-small.jpg">
+                            <span class="icon"></span>
+                            
+                        </a>
+                    </div><div class="mbr-gallery-item">
+                        <a href="#lb-gallery1-22" data-slide-to="3" data-toggle="modal">
+                            <img alt="" src="assets/images/desktop-small.jpg">
+                            <span class="icon"></span>
+                            
+                        </a>
+                    </div><div class="mbr-gallery-item">
+                        <a href="#lb-gallery1-22" data-slide-to="4" data-toggle="modal">
+                            <img alt="" src="assets/images/room-laptop-small.jpg">
+                            <span class="icon"></span>
+                            
+                        </a>
+                    </div><div class="mbr-gallery-item">
+                        <a href="#lb-gallery1-22" data-slide-to="5" data-toggle="modal">
+                            <img alt="" src="assets/images/table-small.jpg">
+                            <span class="icon"></span>
+                            
+                        </a>
+                    </div><div class="mbr-gallery-item">
+                        <a href="#lb-gallery1-22" data-slide-to="6" data-toggle="modal">
+                            <img alt="" src="assets/images/windows-books-small.jpg">
+                            <span class="icon"></span>
+                            
+                        </a>
+                    </div><div class="mbr-gallery-item">
+                        <a href="#lb-gallery1-22" data-slide-to="7" data-toggle="modal">
+                            <img alt="" src="assets/images/working-area-small.jpg">
+                            <span class="icon"></span>
+                            
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+
+    <!-- Lightbox -->
+    <div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery1-22">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <ol class="carousel-indicators">
+                        <li data-app-prevent-settings="" data-target="#lb-gallery1-22" class=" active" data-slide-to="0"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-22" data-slide-to="1"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-22" data-slide-to="2"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-22" data-slide-to="3"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-22" data-slide-to="4"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-22" data-slide-to="5"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-22" data-slide-to="6"></li><li data-app-prevent-settings="" data-target="#lb-gallery1-22" data-slide-to="7"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img alt="" src="assets/images/bike.jpg">
+                        </div><div class="carousel-item">
+                            <img alt="" src="assets/images/code-man.jpg">
+                        </div><div class="carousel-item">
+                            <img alt="" src="assets/images/coworkers.jpg">
+                        </div><div class="carousel-item">
+                            <img alt="" src="assets/images/desktop.jpg">
+                        </div><div class="carousel-item">
+                            <img alt="" src="assets/images/room-laptop.jpg">
+                        </div><div class="carousel-item">
+                            <img alt="" src="assets/images/table.jpg">
+                        </div><div class="carousel-item">
+                            <img alt="" src="assets/images/windows-books.jpg">
+                        </div><div class="carousel-item">
+                            <img alt="" src="assets/images/working-area.jpg">
+                        </div>
+                    </div>
+                    <a class="left carousel-control" role="button" data-slide="prev" href="#lb-gallery1-22">
+                        <span class="icon-prev" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" role="button" data-slide="next" href="#lb-gallery1-22">
+                        <span class="icon-next" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+
+                    <a class="close" href="#" role="button" data-dismiss="modal">
+                        <span aria-hidden="true">×</span>
+                        <span class="sr-only">Close</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section mbr-section__container mbr-section__container--isolated" id="content7-20">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-4 lead"><p>Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. MobiRise Website Builder creates responsive, retina and mobile friendly websites in a few clicks. Mobirise is one of the easiest website development tools available today. It also gives you the freedom to develop as many websites as you like given the fact that it is a desktop app.</p></div>
+            <div class="col-xs-12 col-md-4 lead"><p>Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. MobiRise Website Builder creates responsive, retina and mobile friendly websites in a few clicks. Mobirise is one of the easiest website development tools available today. It also gives you the freedom to develop as many websites as you like given the fact that it is a desktop app.</p></div>
+            <div class="col-xs-12 col-md-4 lead"><p>Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. MobiRise Website Builder creates responsive, retina and mobile friendly websites in a few clicks. Mobirise is one of the easiest website development tools available today. It also gives you the freedom to develop as many websites as you like given the fact that it is a desktop app.</p></div>
+        </div>
+    </div>
+
+</section>
+
+  <script src="assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/bootstrap-carousel-swipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/mobirise-gallery/script.js"></script>
+
+@endsection
