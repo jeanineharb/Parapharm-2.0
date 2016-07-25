0bb4a5af775cfb0900775487b19ac284ffed02d9
Jeanine Harb <jeanine.harb@gmail.com>
2016-07-25 19:13:44 +0300

    Added design elements

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,87 +2,87 @@
 
 @section('content')
 
-<!-- Carousel -->
-<link href="{{ URL::asset('css/carousel.css') }}" rel="stylesheet">
+<section class="mbr-section mbr-section-hero mbr-section-full mbr-parallax-background mbr-section-with-arrow mbr-after-navbar" id="header1-1" style="background-image: url(http://localhost:8000/images/parapharm-2000x1537-43.jpg);">
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-	<li data-target="#myCarousel" data-slide-to="1"></li>
-	<li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner" role="listbox">
-	<div class="item active">
-	  <img class="first-slide" src="http://www.w3schools.com/bootstrap/img_chania2.jpg" alt="First slide">
-	</div>
-	<div class="item">
-	  <img class="second-slide" src="http://www.w3schools.com/bootstrap/img_chania2.jpg" alt="Second slide">
-	  <div class="container">
-		<div class="carousel-caption">
-		  <h1>Another example headline.</h1>
-		  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-		  <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-		</div>
-	  </div>
-	</div>
-	<div class="item">
-	  <img class="third-slide" src="http://www.w3schools.com/bootstrap/img_chania2.jpg" alt="Third slide">
-	  <div class="container">
-		<div class="carousel-caption">
-		  <h1>One more for good measure.</h1>
-		  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-		  <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-		</div>
-	  </div>
-	</div>
-  </div>
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-	<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-	<span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-	<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-	<span class="sr-only">Next</span>
-  </a> 
-</div>
+    <div class="mbr-overlay" style="opacity: 0.1; background-color: rgb(255, 255, 255);"></div>
 
+    <div class="mbr-table-cell">
 
+        <div class="container">
+            <div class="row">
+                <div class="mbr-section col-md-10 col-md-offset-1 text-xs-center">
 
+                    <h1 class="mbr-section-title display-1">PARAPHARM SAL</h1>
+                    <p class="mbr-section-lead lead">Exclusive agents and distributors for dermatological and pharmaceutical products.<br> <br>Having amassed more than three decades of expertise so far, we are known for providing the Lebanese market with the best brands in dermatology and skincare innovations, and for giving our partners a tailor-made coverage and representation in Lebanon.<br></p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="mbr-arrow mbr-arrow-floating" aria-hidden="true"><a href="#features3-2"><i class="mbr-arrow-icon"></i></a></div>
 
-<!-- Marketing messaging and featurettes
-    ================================================== -->
-    <!-- Wrap the rest of the page in another container to center all the content. -->
+</section>
 
-    <div class="container marketing">
+<section class="mbr-cards mbr-section mbr-section-nopadding" id="features3-2" style="background-color: rgb(236, 240, 241);">
 
-      <!-- Three columns of text below the carousel -->
-      <div class="row">
-        <div class="col-lg-3">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-3">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-3">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-3">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-      </div><!-- /.row -->
+    
+
+    <div class="mbr-cards-row row">
+            <div class="mbr-cards-col col-xs-12 col-lg-3" style="padding-top: 120px; padding-bottom: 120px;">
+                <div class="container">
+                  <div class="card cart-block">
+                      <div class="card-img"><img src="assets/images/table.jpg" class="card-img-top"></div>
+                      <div class="card-block">
+                          <h4 class="card-title">Laboratoires NOREVA<br></h4>
+                          <h5 class="card-subtitle">Bootstrap 3 has been noted</h5>
+                          <p class="card-text">Bootstrap 3 has been noted as one of the most reliable and proven frameworks and Mobirise has been equipped to develop websites using this framework.</p>
+                          <div class="card-btn"><a href="https://mobirise.com" class="btn btn-primary">MORE</a></div>
+                      </div>
+                  </div>
+                </div>
+            </div>
+            <div class="mbr-cards-col col-xs-12 col-lg-3" style="padding-top: 120px; padding-bottom: 120px;">
+              <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img"><img src="assets/images/desktop.jpg" class="card-img-top"></div>
+                    <div class="card-block">
+                        <h4 class="card-title">Laboratoires CONTAPHARM</h4>
+                        <h5 class="card-subtitle">One of Bootstrap 3s big points</h5>
+                        <p class="card-text">One of Bootstrap 3's big points is responsiveness and Mobirise makes effective use of this by generating highly responsive website for you.</p>
+                        <div class="card-btn"><a href="https://mobirise.com" class="btn btn-primary">MORE</a></div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="mbr-cards-col col-xs-12 col-lg-3" style="padding-top: 120px; padding-bottom: 120px;">
+              <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img"><img src="assets/images/coworkers.jpg" class="card-img-top"></div>
+                    <div class="card-block">
+                        <h4 class="card-title">BeautyMed</h4>
+                        <h5 class="card-subtitle">Google has a highly exhaustive list of fonts</h5>
+                        <p class="card-text">Google has a highly exhaustive list of fonts compiled into its web font platform and Mobirise makes it easy for you to use them on your website easily and freely.</p>
+                        <div class="card-btn"><a href="https://mobirise.com" class="btn btn-primary">MORE</a></div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="mbr-cards-col col-xs-12 col-lg-3" style="padding-top: 120px; padding-bottom: 120px;">
+              <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img"><img src="assets/images/code-man.jpg" class="card-img-top"></div>
+                    <div class="card-block">
+                        <h4 class="card-title">Onagrine</h4>
+                        <h5 class="card-subtitle">Mobirise gives you the freedom to develop</h5>
+                        <p class="card-text">Mobirise gives you the freedom to develop as many websites as you like given the fact that it is a desktop app.</p>
+                        <div class="card-btn"><a href="https://mobirise.com" class="btn btn-primary">MORE</a></div>
+                    </div>
+                </div>
+              </div>
+            </div>
+    </div>
+</section>
 
 
 
--- resources/views/structure.blade.php
+++ resources/views/structure.blade.php
@@ -14,74 +14,116 @@
 	
 	<!-- Stylesheets -->
 	<link href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-	<link href="{{ URL::asset('font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+	<!-- <link href="{{ URL::asset('font-awesome/css/font-awesome.min.css') }}" rel="stylesheet"> -->
+	<link rel="stylesheet" href="{{ URL::asset('bootstrap-material-design-font/css/material.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('et-line-font-plugin/style.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('tether/tether.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('socicon/css/socicon.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('animate.css/animate.min.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('dropdown/css/style.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('theme/css/style.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('mobirise/css/mbr-additional.css') }}" type="text/css">
 
 	<!-- <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet"> -->
 
 	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900">
 </head>
 
 <body>
+	<section id="menu-5">
+		<nav class="navbar navbar-dropdown navbar-fixed-top">
+			<div class="container">
+				<div class="mbr-table">
+					<div class="mbr-table-cell">
+						<div class="navbar-brand">
+							<a href="{{ url('/') }}" class="navbar-logo"><img src="{{ URL::asset('images/logo-192x128-95.png') }}" alt="Parapharm SAL" title="Parapharm SAL"></a>
+						</div>
+					</div>
+					<div class="mbr-table-cell">
 
+						<button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+							☰
+						</button>
 
-	<nav class="navbar navbar-default navbar-static-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{ url('/') }}">Parapharm SAL</a>
-			</div>
-			<div id="navbar" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="{{ url('/') }}">Home</a></li>
-					<li><a href="{{ url('/about') }}">About</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Products<span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Laboratoires NOREVA</a></li>
-							<li><a href="#">Laboratoires CONTAPHARM</a></li>
-							<li><a href="#">BeautyMed</a></li>
-							<li><a href="#">Onagrine</a></li>
+						<ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar">
+							<li class="nav-item"><a class="nav-link link" href="{{ url('/') }}">HOME</a></li>
+							<li class="nav-item"><a class="nav-link link" href="{{ url('/about') }}">ABOUT</a></li>
+							<li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="#" aria-expanded="false">PRODUCTS</a>
+								<div class="dropdown-menu">
+									<a class="dropdown-item" href="{{ url('/products/noreva') }}">Laboratoires NOREVA</a>
+									<a class="dropdown-item" href="{{ url('/products/contapharm') }}">Laboratoires CONTAPHARM</a>
+									<a class="dropdown-item" href="{{ url('/products/beautymed') }}">BeautyMed</a>
+									<a class="dropdown-item" href="{{ url('/products/onagrine') }}">Onagrine</a>
+								</div>
+							</li>
+							<li class="nav-item"><a class="nav-link link" href="{{ url('/contact') }}">CONTACT</a></li>
+							<li class="nav-item"><a class="nav-link link" href="https://blog.parapharm.com.lb" target="_blank">BLOG</a></li>
 						</ul>
-					</li>
-					<li><a href="{{ url('/contact') }}">Contact</a></li>
-					<li><a href="http://blog.parapharm.com.lb/">Blog</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="https://www.facebook.com/Parapharm" target="_blank"><i class="fa fa-facebook-square"></i></a></li>
-					<li><a href="https://www.linkedin.com/company/parapharm-s-a-l" target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
-					<li><a href="mailto:contact@parapharm.com.lb"><i class="fa fa-envelope"></i></a></li>
-				</ul>
-			</div><!--/.nav-collapse -->
-		</div>
-	</nav>
-	
-	<div class="container">
-	@yield('content')
-	</div>
+						<button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+							☰
+						</button>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</section>
 
-	
-	
-	<!-- Scripts -->
-	<!-- Placed at the end of the document so the pages load faster -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="{{ URL::asset('bootstrap/js/bootstrap.min.js')}}"></script>
+		@yield('content')
 
-	<script type="text/javascript">
-		$(document).ready(function () {
-        $('ul.nav > li').click(function (e) {
-            e.preventDefault();
-            $('ul.nav > li').removeClass('active');
-            $(this).addClass('active');                
-        });            
-    });
-	
-</script>
-</body>
-</html>
+		<section class="mbr-section mbr-section-md-padding mbr-footer footer1" id="contacts1-3" style="background-color: rgb(52, 152, 219); padding-top: 90px; padding-bottom: 90px;">
+
+			<div class="container">
+				<div class="row">
+					<div class="mbr-footer-content col-xs-12 col-md-3">
+						<div><img src="assets/images/logo.png"></div>
+					</div>
+					<div class="mbr-footer-content col-xs-12 col-md-3">
+						<p><strong>Address</strong><br>
+							Parapharm Bldg <br>
+							Libyan Embassy Street <br>
+							Mar Takla, Hazmieh - Lebanon<br>
+							P.O.Box: 77 Hazmieh, Lebanon</p>
+						</div>
+						<div class="mbr-footer-content col-xs-12 col-md-3">
+							<p><strong>Contact</strong><br>
+								Email: contact@parapharm.com.lb<br>
+								Phone: +961 5 454 111 / 333 <br>
+								Fax: +961 5 457 707</p>
+							</div>
+							<div class="mbr-footer-content col-xs-12 col-md-3">
+								<p><strong>Follow Us</strong><br>
+									<a class="nav-link link" href="https://www.facebook.com/Parapharm" target="_blank" aria-expanded="false"><span class="socicon socicon-facebook mbr-iconfont mbr-iconfont-btn"></span></a>
+									<a class="nav-link link" href="https://www.linkedin.com/company/parapharm-s-a-l" target="_blank" aria-expanded="false"><span class="socicon socicon-linkedin mbr-iconfont mbr-iconfont-btn"></span></a>
+									<a class="nav-link link" href="https://blog.parapharm.com.lb" target="_blank" aria-expanded="false"><span class="socicon socicon-wordpress mbr-iconfont mbr-iconfont-btn"></span></a>
+									<a class="nav-link link" href="mailto:contact@parapharm.com.lb" target="_blank" aria-expanded="false"><span class="socicon socicon-mail mbr-iconfont mbr-iconfont-btn"></span></a></p>
+								</div>
+
+							</div>
+						</div>
+					</section>
+
+					<footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-4" style="background-color: rgb(50, 50, 50); padding-top: 1.75rem; padding-bottom: 1.75rem;">
+
+						<div class="container">
+							<p class="text-xs-center">Copyright &copy; 1999 - 2016 Parapharm SAL. All Rights Reserved.</p>
+							<p class="text-xs-center">Site designed by <a href="https://www.linkedin.com/in/jeanineharb" target="_blank">Jeanine Harb</a>.</p>
+						</div>
+					</footer>
+
+
+					<script src="{{ URL::asset('web/assets/jquery/jquery.min.js') }}"></script>
+					<script src="{{ URL::asset('tether/tether.min.js') }}"></script>
+					<script src="{{ URL::asset('bootstrap/js/bootstrap.min.js') }}"></script>
+					<script src="{{ URL::asset('smooth-scroll/SmoothScroll.js') }}"></script>
+					<script src="{{ URL::asset('viewportChecker/jquery.viewportchecker.js') }}"></script>
+					<script src="{{ URL::asset('jarallax/jarallax.js') }}"></script>
+					<script src="{{ URL::asset('dropdown/js/script.min.js') }}"></script>
+					<script src="{{ URL::asset('theme/js/script.js') }}"></script>
+
+
+					<input name="animation" type="hidden">
+				</body>
+				</html>
